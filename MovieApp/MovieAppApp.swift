45f7fe534b52351e18f2572f23e7d41fa5fc54c5
gustavosouto02 <gugasouto2004@gmail.com>
2025-06-27 15:37:17 -0300

    Initial Commit

---
+++ MovieApp/MovieAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieAppApp.swift
+//  MovieApp
+//
+//  Created by Gustavo Souto Pereira on 27/06/25.
+//
+
+import SwiftUI
+
+@main
+struct MovieAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
